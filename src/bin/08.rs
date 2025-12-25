fb865d9b7800eb52438450814be14c515fdbf480
Philipp Panzer <philipp.panzer@ul.com>
2025-12-25 17:05:00 +0100

    add day8 part 2

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -26,9 +26,6 @@ impl Coord {
         ((d.x.pow(2) + d.y.pow(2) + d.z.pow(2)) as f64).sqrt()
     }
 
-    fn connect(self, other: &Self) {
-
-    }
 }
 
 impl From<Vec<i64>> for Coord {
@@ -127,7 +124,58 @@ pub fn part_one(input: &str) -> Option<u64> {
     )
 }
 
-pub fn part_two(input: &str) -> Option<u64> {
+pub fn part_two(input: &str) -> Option<i64> {
+
+    let coords = input.lines().map(parse_line).collect::<Vec<_>>();
+    let mut coord_to_circuit = HashMap::new();
+
+    for (i, &c) in coords.iter().enumerate() {
+        coord_to_circuit.insert(
+            c,
+            CircuitOrRef::Circuit(c, HashSet::from([c])),
+        );
+    }
+
+    for (c1, c2) in coords
+        .iter()
+        .tuple_combinations()
+        .sorted_by(|(a, b), (c, d)|
+            a.straight_line_distance(b).total_cmp(&c.straight_line_distance(d))
+        ) {
+            let s1 = coord_to_circuit.get(c1).unwrap();
+            let s2= coord_to_circuit.get(c2).unwrap();
+
+
+            let c1s = s1.get_containing_circuit();
+            let c2s = s2.get_containing_circuit();
+
+            if c1s != c2s {
+                let set1 = match &coord_to_circuit[&c1s] {
+                    CircuitOrRef::Circuit(_, hs) => hs,
+                    _ => panic!()
+                }.clone();
+                let set2 = match &coord_to_circuit[&c2s] {
+                    CircuitOrRef::Circuit(_, hs) => hs,
+                    _ => panic!()
+                }.clone();
+
+                for c in set2.iter() {
+                    coord_to_circuit.insert(*c, CircuitOrRef::Link(c1s));
+                }
+
+                if set1.len() + set2.len() == coords.len() {
+                    return Some(c1.x * c2.x);
+                }
+
+                let mut new_set = set1;
+                new_set.extend(set2);
+
+                println!("{:?} <--> {:?}, new size = {:?}", c1, c2, new_set.len());
+                coord_to_circuit.insert(c1s, CircuitOrRef::Circuit(c1s, new_set));
+
+            }
+    }
+
     None
 }
 
@@ -144,6 +192,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(25272));
     }
 }
